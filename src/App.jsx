169a8b5e25049983df771aff33436810b234e0d1
laurenhg <laurenhg@gmail.com>
2024-02-18 21:29:49 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AccountSettings from './'
+
+
+function App (){
+    return (
+        <Router>
+            <Routes>
+                Route path="/" element={<Login />} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default App;
